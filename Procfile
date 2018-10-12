09fddbb501c7dd84a25d2c166d809efd59791ed6
Wess58 <wesleymbate@gmail.com>
2018-10-12 12:26:43 +0300

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: ./build/install/Hair-Salon/bin/Hair-Salon
---
+++ src/main/java/App.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+public class App{
+    public static void main(String[] args){
+        staticFileLocation("/public");
+        String layout = "templates/layout.vtl";
+
+//HEROKU
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        setPort(port);
+//end heroku
+    }
+}
