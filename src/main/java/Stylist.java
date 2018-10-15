c6dd92ccccc953a6ab2e0d04707b230a7ae5f742
Wess58 <wesleymbate@gmail.com>
2018-10-15 14:31:39 +0300

    create getters for stylist properties

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -19,5 +19,26 @@ public class Stylist {
         this.email = email;
     }
 
-    
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getSecondName(){
+        return secondName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public String getPhoneNo(){
+        return phoneNo;
+    }
+    public String getIdNo(){
+        return idNo;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public int getId(){
+        return id;
+    }
+  
 }
