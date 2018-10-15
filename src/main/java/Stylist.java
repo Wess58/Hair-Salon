056d20c16c7198d3fbbd850bf4e04aaf567a66f3
Wess58 <wesleymbate@gmail.com>
2018-10-15 14:31:09 +0300

    declared stylist properties and created stylist constructor

---
+++ src/main/java/Stylist.java
@@ -0,0 +1,23 @@
+import org.sql2o.*;
+import java.util.List;
+
+public class Stylist {
+    private String firstName;
+    private String secondName;
+    private String lastName;
+    private String phoneNo;
+    private String idNo;
+    private String email;
+    private int id;
+
+    public Stylist(String firstName, String secondName, String lastName, String phoneNo, String idNo, String email){
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.lastName = lastName;
+        this.phoneNo = phoneNo;
+        this.idNo = idNo;
+        this.email = email;
+    }
+
+    
+}
