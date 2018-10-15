4449c9a8ba809675551a600459764adde60d6f72
Wess58 <wesleymbate@gmail.com>
2018-10-15 14:32:02 +0300

    methods to save stylist in DB

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -40,5 +40,61 @@ public class Stylist {
     public int getId(){
         return id;
     }
-  
+    //DATABASE SASA
+    public static List<Stylist> all(){
+        String sql = "SELECT id, firstName, secondName, lastName, phoneNo, idNo, email" +
+                "FROM stylists";
+        try(Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql).executeAndFetch(Stylist.class);
+        }
+    }
+    public List<Client> getClients() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM clients where stylistId=:id";
+            return con.createQuery(sql)
+                    .addParameter("id", this.id)
+                    .executeAndFetch(Client.class);
+        }
+    }
+    @Override
+    public boolean equals(Object otherStylist) {
+        if (!(otherStylist instanceof Stylist)) {
+            return false;
+        } else {
+            Stylist newStylist = (Stylist) otherStylist;
+            return this.getFirstName().equals(newStylist.getFirstName()) &&
+                    this.getSecondName().equals(newStylist.getSecondName()) &&
+                    this.getLastName().equals(newStylist.getLastName()) &&
+                    this.getPhoneNo().equals(newStylist.getPhoneNo()) &&
+                    this.getIdNo().equals(newStylist.getIdNo()) &&
+                    this.getEmail().equals(newStylist.getEmail()) &&
+                    this.getId() == newStylist.getId();
+        }
+    }
+    public void save() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO stylists (firstName, secondName, lastName, phoneNo, idNo, email) " +
+                    "VALUES (:firstName, :secondName, :lastName, :phoneNo, :idNo, :email)";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("firstName", this.firstName)
+                    .addParameter("secondName", this.secondName)
+                    .addParameter("lastName", this.lastName)
+                    .addParameter("phoneNo", this.phoneNo)
+                    .addParameter("idNo", this.idNo)
+                    .addParameter("email", this.email)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+    public static Stylist find(int id) {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM stylists where id=:id";
+            Stylist stylist = con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Stylist.class);
+            return stylist;
+        }
+    }
+
+
 }
