919d3d028e104005e2b5282da0220faa61f23e7b
Wess58 <wesleymbate@gmail.com>
2018-10-15 14:27:53 +0300

    methods to save clients in DB

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -37,7 +37,49 @@ public class Client {
     public int getStylistId() {
         return stylistId;
     }
-    
+    public static Client find (int id){
+        try(Connection con = DB.sql2o.open() ){
+            String sql = "SELECT * FROM clients where id=:id";
+            Client client = con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Client.class);
+            return client;
+        }
+    }
+    public static List<Client> all() {
+        String sql = "SELECT id, clientFirstName, clientLastName, clientPhoneNo, clientEmail, stylistId FROM clients";
+        try (Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql).executeAndFetch(Client.class);
+        }
+    }
+    @Override
+    public boolean equals(Object otherClient) {
+        if (!(otherClient instanceof Client)) {
+            return false;
+        } else {
+            Client newClient = (Client) otherClient;
+            return this.getClientFirstName().equals(newClient.getClientFirstName()) &&
+                    this.getClientLastName().equals(newClient.getClientLastName()) &&
+                    this.getClientPhoneNo().equals(newClient.getClientPhoneNo()) &&
+                    this.getClientEmail().equals(newClient.getClientEmail()) &&
+                    this.getId() == newClient.getId()  &&
+                    this.getStylistId() == newClient.getStylistId();
+        }
+    }
+    public void save() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO clients (clientFirstName, clientLastName, clientPhoneNo, clientEmail, stylistId) " +
+                    "VALUES (:clientFirstName, :clientLastName, :clientPhoneNo, :clientEmail, :stylistId)";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("clientFirstName", this.clientFirstName)
+                    .addParameter("clientLastName", this.clientLastName)
+                    .addParameter("clientPhoneNo", this.clientPhoneNo)
+                    .addParameter("clientEmail", this.clientEmail)
+                    .addParameter("stylistId", this.stylistId)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
 
 
 }
