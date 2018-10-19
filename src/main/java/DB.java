7653fb34e00de14ce6faf83a1d24c99f79851256
Wess58 <wesleymbate@gmail.com>
2018-10-19 14:26:24 +0300

    pushing DB

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -5,26 +5,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DB {
-  private static URI dbUri;
-   public static Sql2o sql2o;
-   
-   Logger logger = LoggerFactory.getLogger(DB.class);
-   static {
-
-       try {
-           if (System.getenv("DATABASE_URL") == null) {
-               dbUri = new URI("postgres://localhost:5432/hair_salon");
-           } else {
-               dbUri = new URI(System.getenv("DATABASE_URL"));
-           }
-           int port = dbUri.getPort();
-           String host = dbUri.getHost();
-           String path = dbUri.getPath();
-           String username = (dbUri.getUserInfo() == null) ? "postgres" : dbUri.getUserInfo().split(":")[0];
-           String password = (dbUri.getUserInfo() == null) ? "makena58" : dbUri.getUserInfo().split(":")[1];
-           sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
-       } catch (URISyntaxException e ) {
-           logger.error("Unable to connect to database.");
-       }
-   }
+    private static URI dbUri;
+    public static Sql2o sql2o;
+    static Logger logger = LoggerFactory.getLogger(DB.class);
+    static {
+        try {
+            if (System.getenv("DATABASE_URL") == null) {
+                dbUri = new URI("postgres://localhost:5432/hair_salon");
+            } else {
+                dbUri = new URI(System.getenv("DATABASE_URL"));
+            }
+            int port = dbUri.getPort();
+            String host = dbUri.getHost();
+            String path = dbUri.getPath();
+            String username = (dbUri.getUserInfo() == null) ? "postgres" : dbUri.getUserInfo().split(":")[0];
+			String password = (dbUri.getUserInfo() == null) ? "makena58" : dbUri.getUserInfo().split(":")[1];
+			sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
+        } catch (URISyntaxException e ) {
+            logger.error("Unable to connect to database.");
+        }
+    }
 }
